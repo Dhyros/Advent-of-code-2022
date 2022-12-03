8445f5fa9e4bc8cfef67e23627fcc5f11c323027
Dhyros <raulgranados2906@gmail.com>
2022-12-03 03:51:05 +0100

    Day 2

---
+++ src/Day2/Day2.1.cpp
@@ -0,0 +1,106 @@
+/***************************************************************************/
+//
+// RAÚL GRANADOS LÓPEZ
+// 2ºB GII - UGR
+// 
+// Advent of code - 2022
+// 
+// Day 1
+//
+/***************************************************************************/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Types of shapes admitted in the game
+enum struct Shape {
+	Rock = 0,
+	Paper = 1,
+	Scissors = 2
+};
+
+// The shape depends on the char
+Shape CharToShape(char c) {
+	Shape s;
+
+	switch (c) {
+		case 'A':
+		case 'X':
+			s = Shape::Rock;
+			break;
+
+		case 'B':
+		case 'Y':
+			s = Shape::Paper;
+			break;
+
+		case 'C':
+		case 'Z':
+			s = Shape::Scissors;
+	}
+
+	return s;
+}
+
+// Function that returns the outcome of the round
+int Outcome(Shape elfe, Shape me) {
+	int score;
+	// Shows if the first shape wins the second shape
+	bool result[3][3] = {
+		{false, false, true },
+		{true,  false, false},
+		{false, true,  false}
+	};
+
+	// Score of the round
+	if (elfe == me) {
+		score = 3;
+	}
+	else {
+		if (result[(int)me][(int)elfe]) {
+			score = 6;
+		}
+		else {
+			score = 0;
+		}
+	}
+
+	// Add the score of the shape I selected
+	score += (int)me + 1;
+
+	return score;
+}
+
+int main(int argc, char **argv) {
+
+	// Check the amount of arguments
+	if (argc != 2) {
+		cerr <<"Error: Invalida amount of arguments." << endl;
+		cerr << "Uso: Day1 <input>" << endl;
+		exit(-1);
+	}
+
+	// Check if the input file can be opened
+	ifstream fi(argv[1]);
+	if (!fi) {
+		cerr << "Error opening file \"" << argv[1] << "\"." << endl;
+		exit(-1);
+	}
+	
+	// Reading the input file
+	char elfe, me;
+	int score = 0;
+	while (fi >> elfe) {
+		fi >> me;
+
+		// Add the outcome of the current round
+		score += Outcome(CharToShape(elfe), CharToShape(me));
+	}
+
+	// Showing the final result
+	cout << "Total score of " << score << endl;
+
+	return 0;
+}
---
+++ src/Day2/Day2.2.cpp
@@ -0,0 +1,100 @@
+/***************************************************************************/
+//
+// RAÚL GRANADOS LÓPEZ
+// 2ºB GII - UGR
+// 
+// Advent of code - 2022
+// 
+// Day 1
+//
+/***************************************************************************/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Types of shapes admitted in the game
+enum struct Shape {
+	Rock = 0,
+	Paper = 1,
+	Scissors = 2
+};
+
+// The shape depends on the char
+Shape CharToShape(char c) {
+	Shape s;
+
+	switch (c) {
+		case 'A':
+			s = Shape::Rock;
+			break;
+
+		case 'B':
+			s = Shape::Paper;
+			break;
+
+		case 'C':
+			s = Shape::Scissors;
+	}
+
+	return s;
+}
+
+// Function that returns the outcome of the round
+int Outcome(char elfe, char me) {
+	int score;
+	// Shows if the first shape wins the second shape
+	bool result[3][3] = {
+		{false, false, true },
+		{true,  false, false},
+		{false, true,  false}
+	};
+
+	// Score of the round
+	if (me == 'Y') {
+		score = 3 + (int)CharToShape(elfe)+1;
+	}
+	else {
+		if (me == 'Z') {
+			score = 6 + ((int)CharToShape(elfe) + 1) % 3 + 1;
+		}
+		else {
+			score = 0 + ((int)CharToShape(elfe) + 2) % 3 + 1;
+		}
+	}
+
+	return score;
+}
+
+int main(int argc, char **argv) {
+
+	// Check the amount of arguments
+	if (argc != 2) {
+		cerr <<"Error: Invalida amount of arguments." << endl;
+		cerr << "Uso: Day1 <input>" << endl;
+		exit(-1);
+	}
+
+	// Check if the input file can be opened
+	ifstream fi(argv[1]);
+	if (!fi) {
+		cerr << "Error opening file \"" << argv[1] << "\"." << endl;
+		exit(-1);
+	}
+	
+	// Reading the input file
+	char elfe, me;
+	int score = 0;
+	while (fi >> elfe) {
+		fi >> me;
+
+		// Add the outcome of the current round
+		score += Outcome(elfe, me);
+	}
+
+	// Showing the final result
+	cout << "Total score of " << score << endl;
+
+	return 0;
+}
